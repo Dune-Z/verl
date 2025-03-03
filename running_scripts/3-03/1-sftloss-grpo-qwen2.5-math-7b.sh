0126a87364a53fa0742b554efa9258423b49fabd
Dune-Z <dune.ustc@gmail.com>
2025-03-02 22:32:03 -0600

    submit reinforce++ and math-only scripts

--- running_scripts/3-03/1-sftloss-grpo-qwen2.5-math-7b.sh
+++ running_scripts/3-03/1-sftloss-grpo-qwen2.5-math-7b.sh
@@ -1,16 +1,16 @@
 #set -x
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+#export CUDA_VISIBLE_DEVICES=6,7,8,9
 
 ### task name can be selected from [gsm8k, math_dataset, opencoder]
 TASK_NAME=prime
-MATH_ONLY=False
+MATH_ONLY=True
 # comment START_IDX and END_IDX if you want to use the whole dataset for the training
-sft_loss_coef=0.2
+sft_loss_coef=0.5
 REMOTE_DATA_PATH=PRIME-RL/Eurus-2-RL-Data
 SAVE_LOCAL_DIR_PREFIX='checkpoints/'
-PROJECT_NAME=Qwen2.5-Math-0.5B
-MODEL_NAME=Qwen/Qwen2.5-0.5B
-EXPERIMENT_NAME=DEBUG
+PROJECT_NAME=Qwen2.5-Math-7B
+MODEL_NAME=Qwen/Qwen2.5-Math-7B
+EXPERIMENT_NAME=grpo-sftloss-coef${sft_loss_coef}
 SAVE_LOCAL_DIR=${SAVE_LOCAL_DIR_PREFIX}${PROJECT_NAME}/${EXPERIMENT_NAME}
 
 ### preprocess the dataset
@@ -27,6 +27,7 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 # yifei's key
 export WANDB_API_KEY=d61cd005c38e0e1e27d921c951303410316ac718
 python3 -m verl.trainer.main_ppo \
+    actor_rollout_ref.actor.sft_loss_coef=${sft_loss_coef} \
     algorithm.reward_scale=1. \
     algorithm.reward_offset=-1. \
     algorithm.adv_estimator=grpo \
@@ -37,7 +38,7 @@ python3 -m verl.trainer.main_ppo \
     data.train_batch_size=1024 \
     data.val_batch_size=1024 \
     data.max_prompt_length=1024 \
-    data.max_response_length=128 \
+    data.max_response_length=3072 \
     actor_rollout_ref.model.path=${MODEL_NAME} \
     actor_rollout_ref.actor.optim.lr=5e-7 \
     actor_rollout_ref.model.use_remove_padding=True \
@@ -48,12 +49,12 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
-    actor_rollout_ref.rollout.n=1 \
+    actor_rollout_ref.rollout.n=5 \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
-    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=4800 \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
