0fedfcbd65a6b2819bbf320a629c4c9c9546235e
Dune-Z <dune.ustc@gmail.com>
2025-03-04 14:03:55 -0600

    double lr

--- bd_scripts/3-04/ppo-qwen2.5-math-7b.sh
+++ bd_scripts/3-04/ppo-qwen2.5-math-7b.sh
@@ -55,7 +55,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.path=${MODEL_NAME} \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
-    actor_rollout_ref.actor.optim.lr=5e-7 \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.actor.use_kl_loss=True \
