7104fc10f5695b6a65b7341d6a33be164b333ff7
Dune-Z <dune.ustc@gmail.com>
2025-03-08 20:14:25 -0600

    add gpu usage for vllm and tp size

--- bd_scripts/3-07/ppo-qwen2.5-7b.sh
+++ bd_scripts/3-07/ppo-qwen2.5-7b.sh
@@ -63,9 +63,9 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
-    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
     actor_rollout_ref.rollout.name=vllm \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
     actor_rollout_ref.rollout.n=1 \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
