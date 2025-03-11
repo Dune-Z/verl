b99056eada779764dc35491ea9d5a7fb9aef8935
02lyx <yuanxinliu2029@u.northwestern.edu>
2025-03-11 16:40:17 -0500

    revise script to dynamic batch

--- bd_scripts/3-11/ppo-qwen2.5-7b.sh
+++ bd_scripts/3-11/ppo-qwen2.5-7b.sh
@@ -61,7 +61,6 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.actor.ppo_mini_batch_size=1024 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0 \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
@@ -72,9 +71,11 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.n=64 \
-    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    actor_rollout_ref.actor.use_dynamic_bsz=True \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=24000 \
+    actor_rollout_ref.rollout.log_prob_max_token_len_per_gpu=24000 \
+    actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=24000 \
     critic.optim.lr=5e-6 \
     critic.optim.lr_warmup_steps_ratio=0.03 \
     critic.optim.warmup_style="constant" \
@@ -82,9 +83,9 @@ python3 -m verl.trainer.main_ppo \
     critic.model.path=${MODEL_NAME} \
     critic.model.enable_gradient_checkpointing=True \
     critic.ppo_mini_batch_size=128 \
-    critic.ppo_micro_batch_size_per_gpu=16 \
     critic.model.fsdp_config.param_offload=False \
     critic.model.fsdp_config.optimizer_offload=False \
+    critic.ppo_max_token_len_per_gpu=72000 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name=${PROJECT_NAME} \
