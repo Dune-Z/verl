da6852a2eafe5f465fc8fc6fa450a876cf5524a5
Dune-Z <dune.ustc@gmail.com>
2025-03-11 23:20:05 -0500

    remove 2- scipt head

--- running_scripts/3-09/2-ppo7k.sh
+++ running_scripts/3-09/2-ppo7k.sh
@@ -1,9 +1,3 @@
-wget -qO- https://astral.sh/uv/install.sh | sh
-uv venv briter --python 3.11 && source briter/bin/activate && uv pip install --upgrade pip --link-mode=copy
-uv pip install -r requirements.txt --link-mode=copy
-uv pip install flash_attn --no-build-isolation --link-mode=copy
-uv uninstall wandb
-uv pip install wandb --no-cache-dir --link-mode=copy
 export WANDB_API_KEY=d61cd005c38e0e1e27d921c951303410316ac718
 wandb login --relogin $WANDB_API_KEY
 
