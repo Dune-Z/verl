07583cdadb6f6a82710afbd525b52f2822479cc6
Dune-Z <dune.ustc@gmail.com>
2025-03-26 20:14:22 -0500

    add debug

--- sft_scripts/3-26/mix-math-gen-debug.sh
+++ sft_scripts/3-26/mix-math-gen-debug.sh
@@ -1,8 +1,17 @@
 set -x
+wget -qO- https://astral.sh/uv/install.sh | sh
+uv venv briter --python 3.11 && source briter/bin/activate && uv pip install --upgrade pip --link-mode=copy
+uv pip install -r requirements.txt --link-mode=copy
+uv pip install flash_attn --no-build-isolation --link-mode=copy
+uv uninstall wandb
+uv pip install wandb --no-cache-dir --link-mode=copy
+uv pip install math_verify --no-build-isolation --link-mode=copy
+uv pip install transformers==4.47.1 deepspeed
 export HYDRA_FULL_ERROR=1
 export VLLM_ATTENTION_BACKEND=XFORMERS
 export WANDB_API_KEY=d61cd005c38e0e1e27d921c951303410316ac718
 wandb login --relogin $WANDB_API_KEY
+export HF_PATH=Yuanxin-Liu/${PROJECT_NAME}-${EXPERIMENT_NAME}
 
 MODEL_NAME=Qwen/Qwen2.5-7B
 
@@ -16,16 +25,16 @@ python3 data_preprocess/math_r1_500.py
 if [ -d "./data/mix-math" ]; then
     rm -rf ./data/mix-math
 fi
-python3 data_preprocess/create_math_data_mix.py --local_dir data/mix-math/train.parquet --sample_start_idx 0 --sample_end_idx 128
+python3 data_preprocess/create_math_data_mix.py --local_dir data/mix-math/train.parquet --sample_start_idx 0 --sample_end_idx 1024
 
 
-CUDA_VISIBLE_DEVICES=0,1 python3 -m verl.trainer.main_generation \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m verl.trainer.main_generation \
     trainer.nnodes=1 \
-    trainer.n_gpus_per_node=2 \
+    trainer.n_gpus_per_node=8 \
     data.path=./data/mix-math/train.parquet \
     data.prompt_key=prompt \
     data.n_samples=2 \
-    data.output_path=./data/mix-math/generation.parquet \
+    data.output_path=./data/mix-math/generation-debug.parquet \
     model.path=$MODEL_NAME \
     +model.trust_remote_code=True \
     rollout.temperature=1.0 \
@@ -36,3 +45,6 @@ CUDA_VISIBLE_DEVICES=0,1 python3 -m verl.trainer.main_generation \
     rollout.tensor_model_parallel_size=2 \
     rollout.gpu_memory_utilization=0.8
 
+python3 sft_scripts/debug/generation_hub.py --push \
+    --datafiles ./data/mix-math/generation-debug.parquet \
+    --hub Yuanxin-Liu/mix-math-7b-Qwen-rs-debug
